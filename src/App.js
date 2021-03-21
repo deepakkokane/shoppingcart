5c3bbf711f3d382218c5c3148256bf20c0f7f5c2
deepakkokane <deepak.kokane455@gmail.com>
2021-03-21 18:14:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Container, Col, Row } from "reactstrap";
+import { ToastContainer, toast } from "react-toastify";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import BuyPage from "./components/BuyPage";
+import Cart from "./components/Cart";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cartItem, setCartItem] = useState([]);
+
+    const addCartItem = (item) => {
+        const alreadyInCart = cartItem.findIndex(
+            (singleitem) => singleitem.id === item.id
+        );
+
+        if (alreadyInCart !== -1) {
+            return toast("already in cart", { type: "error" });
+        }
+        setCartItem([...cartItem, item]);
+    };
+
+    const removeCartItem = (item) => {
+        setCartItem(cartItem.filter((aitem) => aitem.id !== item.id));
+    };
+
+    const buyNow = () => {
+        toast("Purchase Complete", {
+            type: "success",
+        });
+        setCartItem([]);
+    };
+
+    return (
+        <Container >
+          <ToastContainer/>
+            <Row>
+                <Col md={8}>
+                <BuyPage addCartItem={addCartItem}></BuyPage>
+                </Col>
+                <Col md={4}>
+                  <Cart removeCartItem={removeCartItem} cartItems={cartItem} buyNow={buyNow} />
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default App;
